552b35a12788ca42c89b6257dd73928cb4841717
core23 <mail@core23.de>
2019-03-31 01:24:25 +0100

    [PATCH] Catch most specific exceptions first

--- src/Connection/HttplugConnection.php
+++ src/Connection/HttplugConnection.php
@@ -61,10 +61,10 @@ final class HttplugConnection implements ConnectionInterface
 
         try {
             $response = $this->client->sendRequest($request);
-        } catch (Exception $exception) {
-            throw new MatomoException('Error calling Matomo API.', 500, $exception);
         } catch (ClientException $exception) {
             throw new MatomoException('Error calling Matomo API.', $exception->getCode(), $exception);
+        } catch (Exception $exception) {
+            throw new MatomoException('Error calling Matomo API.', 500, $exception);
         }
 
         if (200 !== $response->getStatusCode()) {
