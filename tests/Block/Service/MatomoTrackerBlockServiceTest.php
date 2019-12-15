0c99e37b5728617d7b18f9b863b50e6ee7183774
core23 <mail@core23.de>
2019-12-15 10:33:43 +0100

    Drop Sonata CoreBundle dependency

--- tests/Block/Service/MatomoTrackerBlockServiceTest.php
+++ tests/Block/Service/MatomoTrackerBlockServiceTest.php
@@ -15,8 +15,8 @@ use Core23\MatomoBundle\Block\Service\MatomoTrackerBlockService;
 use Sonata\BlockBundle\Block\BlockContext;
 use Sonata\BlockBundle\Form\Mapper\FormMapper;
 use Sonata\BlockBundle\Model\Block;
-use Sonata\BlockBundle\Model\BlockInterface;
 use Sonata\BlockBundle\Test\BlockServiceTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 final class MatomoTrackerBlockServiceTest extends BlockServiceTestCase
 {
@@ -33,19 +33,30 @@ final class MatomoTrackerBlockServiceTest extends BlockServiceTestCase
             'template'    => '@Core23Matomo/Block/block_matomo_tracker.html.twig',
         ]);
 
-        $blockService = new MatomoTrackerBlockService($this->templating);
-        $blockService->execute($blockContext);
+        $response = new Response();
 
-        static::assertSame('@Core23Matomo/Block/block_matomo_tracker.html.twig', $this->templating->view);
+        $this->twig->expects(static::once())->method('render')
+            ->with(
+                '@Core23Matomo/Block/block_matomo_tracker.html.twig',
+                [
+                    'context'    => $blockContext,
+                    'settings'   => $blockContext->getSettings(),
+                    'block'      => $blockContext->getBlock(),
+                    'statusCode' => 200,
+                ]
+            )
+            ->willReturn('TWIG_CONTENT')
+        ;
 
-        static::assertSame($blockContext, $this->templating->parameters['context']);
-        static::assertIsArray($this->templating->parameters['settings']);
-        static::assertInstanceOf(BlockInterface::class, $this->templating->parameters['block']);
+        $blockService = new MatomoTrackerBlockService($this->twig);
+
+        static::assertSame($response, $blockService->execute($blockContext, $response));
+        static::assertSame('TWIG_CONTENT', $response->getContent());
     }
 
     public function testDefaultSettings(): void
     {
-        $blockService = new MatomoTrackerBlockService($this->templating);
+        $blockService = new MatomoTrackerBlockService($this->twig);
         $blockContext = $this->getBlockContext($blockService);
 
         $this->assertSettings([
@@ -60,7 +71,7 @@ final class MatomoTrackerBlockServiceTest extends BlockServiceTestCase
 
     public function testGetMetadata(): void
     {
-        $blockService = new MatomoTrackerBlockService($this->templating);
+        $blockService = new MatomoTrackerBlockService($this->twig);
 
         $metadata = $blockService->getMetadata();
 
@@ -74,7 +85,7 @@ final class MatomoTrackerBlockServiceTest extends BlockServiceTestCase
 
     public function testConfigureEditForm(): void
     {
-        $blockService = new MatomoTrackerBlockService($this->templating);
+        $blockService = new MatomoTrackerBlockService($this->twig);
 
         $block = new Block();
 
